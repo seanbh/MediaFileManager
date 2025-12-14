6e20341cd9aeabdb0d9f14650a3ef918dd57d517
seanbh <seanbh@users.noreply.github.com>
2025-12-13 18:52:25 -0600

    w

--- Program.cs
+++ Program.cs
@@ -1,8 +1,8 @@
 ﻿/*****ALWAYS SET THE VARIABLES BELOW THIS LINE********/
 string videoDirectoryPath = @$"C:\Users\seanh\Pictures\Video Projects\Stage\THESE_HAVE_BEEN_COMBINED_INTO_MPEGS\2025";
 string photoDirectoryPath = $@"F:\Pictures\2025";
-// string videoDirectoryPath = @"C:\Users\seanh\Pictures\Google Takeout\Takeout\Google Photos\Photos from 2025";
-// string photoDirectoryPath = @"C:\Users\seanh\Pictures\Google Takeout\Takeout\Google Photos\Photos from 2025";
+//string videoDirectoryPath = @"C:\Users\seanh\Downloads\test";
+//string photoDirectoryPath = @"C:\Users\seanh\Downloads\test";
 ProcessType processType = ProcessType.EndOfYear;
 /******ALWAYS SET THE VARIABLES ABOVE THIS LINE********/
 
